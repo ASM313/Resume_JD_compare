14875e0dd4505cccdf4974ccdb8e0286105523b3
Atiq Mansoori <er_asm@outlook.com>
2024-08-31 10:42:13 +0530

    Utilities written

--- utils.py
+++ utils.py
@@ -2,3 +2,36 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 import PyPDF2 as pdf
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Gemini Response
+
+def get_gemini_response(input):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(input)
+    
+    return response.text 
+
+# Extract text from pdf
+def pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text += str(page.extract_text())
+        
+    return text        
+
+# Prompt Template
+prompt="""
+        Act as a Skilled and very experiended ATS(Application Tracking System) with a deep understading of IT field, software engineering, data science, data analysis, big data engineering, Generative ai etc. Your task is to evaluate the resume based on given job description. Consider that job market is very compititive and you should provide best assistance for improving the resumes. Assign percentage matching based on job description and the missing keywords with high accuracy.
+        This is resume : {text} 
+        This is job description: {jd}
+        
+        give me response in this format{{"Matching Percent": "%","Missing Keywords": [],
+        "Profile Summary":" "
+        }}
+"""
+
